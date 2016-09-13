c51262bf6e1470362f1e4eed3982ea767c827c65
jetflare <kunchyi1@gmail.com>
2016-09-13 11:01:17 +0800

    Update greek.js

--- greek.js
+++ greek.js
@@ -36,6 +36,6 @@ function appendToGreekTable(index){
 
 greekUpperCase.forEach(appendToGreekTable)
 
-function loadGreekTable(){
+//function loadGreekTable(){
 	document.getElementById('characterTable').appendChild(greekTable)
-}
+//}
